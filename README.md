0ec2018cf47918f8bb7805d901c8bfa1cd1754d5
abdalla93 <abdallah.hesham.m@gmail.com>
2024-06-30 13:34:31 +0300

    set up db

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 -[x] Make it deploy
 -[x] scaffold basic ui with mock data
 -[x] Tidy up build process ...
--[] actually set up a database
+-[x] actually set up a database
 -[] attach databast to UI
 -[] add authentication
 -[] add image upload
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/6b02abda-8456-4533-bc3f-aabec1604fad-hijv4i.jpg",
   "https://utfs.io/f/bfc47fce-7849-41e3-b693-eb45f0d42fd9-jlo1ag.jpeg",
   "https://utfs.io/f/9bf675a4-9997-48af-8e03-6cd7c0f6ca8a-nlisfc.jpeg",
-  "https://utfs.io/f/38e34072-903c-408b-9db5-21825ff27bb3-iuzhru.webp"
+  "https://utfs.io/f/38e34072-903c-408b-9db5-21825ff27bb3-iuzhru.webp",
 ];
-const mockImages = mockUrls.map((url,index) => ({
-  id:index + 1,
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
   url,
-}))
-export default function HomePage() {
+}));
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log("posts", posts);
+
   return (
     <main className="">
-      <div className="flex flex-wrap gab-4">
-        {
-          [...mockImages,...mockImages,...mockImages].map(image => (
-            <div  key={image.id} className="w-48">
-              <img src={image.url} alt="image" className="w-40 h-40 m-2"/>
-            </div>
-          ))
-        }
+      <div className="gab-4 flex flex-wrap">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
+            <img src={image.url} alt="image" className="m-2 h-40 w-40" />
+          </div>
+        ))}
       </div>
     </main>
   );
